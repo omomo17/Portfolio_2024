9e2c41d6cfda769c43dfa71556d494b8bf9dc34c
momo <omomo061017@gmail.com>
2024-08-26 22:18:57 +0900

    updated.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import './index.css';
 
 const App = () => {
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+    {/* <div className="absolute top-0 z-[-2] h-screen w-screen transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div> */}
+    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
     {/* <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"> */}
       {/* <h1 className="text-3xl font-bold underline">React App</h1> */}
       <NavBar />
+   
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
         //         </ul>
         //     </div>
         // </nav>
-        <nav className="bg-gray-800 mb-20 flex justify-between items-center py-6">
+        <nav className="mb-20 flex justify-between items-center py-6">
             <div className="flex flex-shrink-0 items-center">
                 <img src={logo} alt="logo" />
             </div>
