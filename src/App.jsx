1c5c3eb804c7313dc68f61b16f648b8dd9b1a9b8
momo <omomo061017@gmail.com>
2024-08-27 07:43:04 +0900

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 // import React from "react";
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
+import About from './components/About';
 import './index.css';
 
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <div className='fixed top-0 -z-10 h-full w-full'></div>
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
-      <div className='container mx-auto px-2'>
-      <NavBar />
-      <Hero />
+      <div className='fixed top-0 -z-10 h-full w-full'>
+        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
-    </div>
+      <div className='container mx-auto px-2'>
+        <NavBar />
+        <Hero />
+        <About />
+      </div>
+      </div>
   );
 }
 
