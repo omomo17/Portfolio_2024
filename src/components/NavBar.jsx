4dea817e59d3ff694fedde21ec6be78d92ce88cb
omomo <omomo1017@gmail.com>
2024-08-27 09:23:09 +0900

    finished About me

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     return (
         <nav className="mb-20 flex justify-between items-center py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-10" src={logo} alt="logo" />
+                {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
             </div>
             <div className="m-8 flex item-center justify-center gap-4 text-2xl">
                 <FaLinkedin />
