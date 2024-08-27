00e57284d858ccf49242499e58c428ed20a02853
omomo <omomo1017@gmail.com>
2024-08-28 08:53:00 +0900

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,14 @@ import { FaInstagram } from "react-icons/fa";
 const NavBar = () => {
     return (
         <nav className="mb-20 flex justify-between items-center py-6">
-            <div className="flex flex-shrink-0 items-center">
-                {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
-            </div>
-            <div className="m-8 flex item-center justify-center gap-4 text-2xl">
-                <FaLinkedin />
-                <FaGithub />
-                <FaSquareXTwitter />
-                <FaInstagram />
+            <div className="flex flex-shrink-0 items-center"></div>
+            <div className="m-8 flex item-center justify-center gap-4 text-3xl">
+            <a href="https://github.com/omomo17" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                    <FaGithub className="hover:text-gray-600 transition-colors duration-200" />
+                </a>
+                <a href="https://www.instagram.com/omomo_aa" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                    <FaInstagram className="hover:text-pink-500 transition-colors duration-200" />
+                </a>
             </div>
         </nav>
     );
